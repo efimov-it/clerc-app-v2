71a40c1da1efeb6463924a5f02c037d7a31644d3
Petr <mail@efimov-it.info>
2020-05-09 16:00:40 +0300

    Build a project for GitHub Pages

---
+++ docs/precache-manifest.1f4505e8625a0f63813e3ed02d99f11f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbe3ec32afb387996a4d",
+    "url": "/css/app.3d2404db.css"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/fonts/Roboto-Bold.e07df86c.ttf"
+  },
+  {
+    "revision": "88823c2015ffd5fa89d567e17297a137",
+    "url": "/fonts/Roboto-Light.88823c20.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/fonts/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "0a9e43b0b456206cd4bb1f16b13ffc08",
+    "url": "/img/back-arrow.0a9e43b0.svg"
+  },
+  {
+    "revision": "fd37c5749b52c56531726d8a7ec63335",
+    "url": "/img/logout-button.fd37c574.svg"
+  },
+  {
+    "revision": "f0d12703ebbf00cbeaa9eed8d474f77c",
+    "url": "/img/remove.f0d12703.svg"
+  },
+  {
+    "revision": "2bda1680ef9e110cb75b45840a23f3fc",
+    "url": "/img/users-button.2bda1680.svg"
+  },
+  {
+    "revision": "b0ae3e641c91eb8bbc803079d492ddc1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dbe3ec32afb387996a4d",
+    "url": "/js/app.36624ffa.js"
+  },
+  {
+    "revision": "88e79e657d9666d21732",
+    "url": "/js/chunk-vendors.c9513eec.js"
+  },
+  {
+    "revision": "efe323aa63bc17ca49194b545c9dcc48",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
